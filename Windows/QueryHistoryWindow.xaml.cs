d5491a091b98524b4059b9ef3ce00fe084ad1986
rasimin <rasimin@praisindo.co.id>
2026-08-06 17:58:52 +0700

    style(ui): redesign QueryHistoryWindow with auto-close and unified dark layout

--- Windows/QueryHistoryWindow.xaml.cs
+++ Windows/QueryHistoryWindow.xaml.cs
@@ -175,6 +175,32 @@ namespace SSMS
             }
         }
 
+        private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
         private void HistoryGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             OpenSelectedEntry();
@@ -190,6 +216,7 @@ namespace SSMS
             if (HistoryGrid.SelectedItem is QueryHistoryEntry entry)
             {
                 OpenInNewQueryRequested?.Invoke(this, entry);
+                Close();
             }
         }
     }
